f29670776c7a0fc4b5082ae7bfb456dfcad97890
jenniferyingg <jenniferxying@gmail.com>
2023-09-02 17:16:22 -0400

    changed neo4j config to have env variables

---
+++ config/config.ts
@@ -0,0 +1,14 @@
+// config/config.ts
+interface Neo4jConfig {
+  uri: string;
+  username: string;
+  password: string;
+}
+
+const neo4jConfig: Neo4jConfig = {
+  uri: process.env.NEO4J_URI,
+  username: process.env.NEO4J_USERNAME,
+  password: process.env.NEO4J_PASSWORD,
+};
+
+export default neo4jConfig;
